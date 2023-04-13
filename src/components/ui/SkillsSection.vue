e17a3e4c89b2253f4f9fb104f7e1ef2dd66026b5
Saymo23 <daniel.santarriaga@gmail.com>
2023-04-13 16:53:57 -0600

    Fix Skills: Arrow Flip

--- src/components/ui/SkillsSection.vue
+++ src/components/ui/SkillsSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="skills__content" :class="{ 'skills__close': is_active_skill }">
+  <div class="skills__content" >
     <div class="skills__header "  @click="toggleOpen()">
       <i class="uil skills__icon" :class="icon"></i><!-- /.uil skills__icon -->
 
@@ -8,7 +8,7 @@
         <span class="skills__subtitle">{{ subtitle }}</span><!-- /.skills__sbutitle -->
       </div>
 
-      <i class="uil uil-angle-down skills__arrow"></i><!-- /.uil uil-angle-down skills__arrow -->
+      <i class="uil uil-angle-down skills__arrow" :class="{'flip180': state.isOpen, 'flip0': !state.isOpen}"></i><!-- /.uil uil-angle-down skills__arrow -->
     </div><!-- /.skills__header -->
     <div class="skills__list grid" :class="{ 'height-transition': true, 'open': state.isOpen }">
       <SkillsData :skills="skills"/>
@@ -24,17 +24,8 @@ import SkillsData from './SkillsData.vue';
 export default {
     props: ["title", "subtitle", "icon", "skills"],
     components: { SkillsData },
-    methods:{
-      toggleSkill(){
-        this.is_active_skill = !this.is_active_skill
-        console.log(this.is_active_skill);
-      }
-    },
-    data(){
-      return {
-        is_active_skill: true
-      }
-    },
+    
+    
     setup(){
       const state = reactive({
         isOpen : false
