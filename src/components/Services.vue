1f3aba566533cd21a24617303035cb5cdae76ab3
Saymo23 <daniel.santarriaga@gmail.com>
2023-04-17 12:11:28 -0600

    Services completed

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -2,47 +2,51 @@
   <h2 class="section__title">Services</h2><!-- /.section__title -->
   <span class="section__subtitle">What it offer</span><!-- /.section__subtitle -->
 
-  <div class="services__content">
-    <div>
-      <i class="uil uil-web-grid services__icon"></i><!-- /.uil uil-web-grid services__icon -->
-      <h3 class="services__title">
-        Frontend <br>Project
-      </h3><!-- /.services__title -->
-      
-      <span class="button button--flex button--small button--link services__button">
-        View More
-        <i class="uil uil-arrow-right services__icon"></i>
-      </span><!-- /.button button--flex button--small button--link services__button -->
-      
-      <div class="services__modal">
-        <div class="services__modal-content">
-          <div class="services__modal-title">
-            <h4>
-              Frontend Project
-            </h4>
-            <i class="uil uil-times services__modal-close"></i><!-- /.uil uil-times services__modal-close -->
-            <ul class="services__modal-services grid">
-              <li class="services__modal-service">
-                <i class="uil uil-check-circle services__modal-icon"></i>
-                <p>I develop the user interface. </p>
-                <p>Web page development.</p>
-                <p>I position your company brand.</p>
-              </li><!-- /.services__modal-service -->
+  <div class="services__container container grid">
+     
+    <Service v-for="s in service" :service="s" :key="s.title" />
 
-            </ul><!-- /.services__modal-services grid -->
-          </div><!-- /.services__modal-title -->
-        </div><!-- /.services__modal-content -->
-      </div><!-- /.services__modal -->
-
-    </div>
-
-  </div><!-- /.services__content -->
+  </div><!-- /.services__container -->
 
 </template>
 
-<script>
+<script >
+import Service from './ui/Service.vue'
 export default {
-
+  components: { Service },
+  data: () => {
+    return {
+      service: [
+        {
+          title: 'Frontend <br>Developer',
+          icon: 'uil-arrow',
+          points: [
+            'I develop the user interface.',
+            'Web page development.',
+            'I position your company brand.'
+          ]
+        },
+        {
+          title: 'Backend <br>Developer',
+          icon: 'uil-servers',
+          points: [
+            'I build web server for your requirements.',
+            'REST API: Nest.js and Adonis.js',
+            'Configure DNS, domains & WHM/cPanel servers.'
+          ]
+        },
+        {
+          title: 'Wordpress <br>Developer',
+          icon: 'uil-wordpress-simple',
+          points: [
+            'I develop your blog web page in Wordpress.',
+            'Handcrafted Themes and optimized.',
+            'I develop custom plugins'
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/ui/Service.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="services__content">
+    <div>
+      <i class="uil services__icon" :class="service.icon"></i>
+      <h3 class="services__title" v-html="service.title"></h3>
+    </div>
+      
+    <span class="button button--flex button--small button--link services__button" @click="showModal()">
+      View More
+      <i class="uil uil-arrow-right service__icon button__icon"></i>
+    </span><!-- /.button button--flex button--small button--link services__button -->
+    
+    <div class="services__modal " :class="{'services__modal-active': state.is_modal_active}">
+      <div class="services__modal-content">
+        <h4 class="services__modal-title" v-html="service.title"></h4>
+        <i class="uil uil-times services__modal-close" @click="showModal()"></i><!-- /.uil uil-times services__modal-close -->
+        <ul class="services__modal-services grid">
+          <li v-for="p in service.points" :key="p" class="services__modal-service">
+            <i class="uil uil-check-circle services__modal-icon"></i> 
+            <p >{{ p }}</p>
+          </li><!-- /.services__modal-service -->
+        </ul><!-- /.services__modal-services grid -->
+      </div><!-- /.services__modal-content -->
+    </div><!-- /.services__modal -->
+
+  </div><!-- /.services__content -->
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  props: ['service'],
+  data: () => {
+    return {
+      is_modal_active: false
+    }
+  },
+  setup(){
+    const state = reactive({
+      is_modal_active : false
+    });
+
+    const showModal = () => {
+      console.log(state.is_modal_active);
+      state.is_modal_active = !state.is_modal_active
+    }
+    return { state, showModal }
+  }
+}
+</script>
+
+<style>
+
+</style>
